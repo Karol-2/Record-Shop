48c2613d4b8744c5787b4f9ab25294eb8a45a6e8
Karol Krawczykiewicz <26karol26@gmail.com>
2023-10-03 20:32:39 +0200

    zad1 fix

--- lab01/zad1.js
+++ lab01/zad1.js
@@ -1,12 +1,27 @@
+async function fetchData(id) {
+  const userRes = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  const user = await userRes.json();
 
+  if (userRes.status === 404) {
+    console.log(`Nie znaleziono użytkownika o id ${id}`);
+    return;
+  }
 
-async function fetchData(id){
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-    .then((res)=>{
-        console.log(res.json())
-    }).catch(()=>{
-        console.log(`Nie znaleziono użytkownika o id ${id}`)
-    })
+  const postsRes = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+  );
+  const posts = await postsRes.json();
+
+  const result = {
+    userId: user.id,
+    name: user.name,
+    posts: posts.map((post) => {
+      return { postId: post.id,title: post.title, body: post.body };
+    }),
+  };
+  console.log(result);
 }
 
-fetchData(5)
+fetchData(5);
