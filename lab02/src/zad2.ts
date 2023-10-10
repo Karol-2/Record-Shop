af42ff20b7fbe3dd5054c1fcccbb220fdedcaaf0
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-10-10 19:24:43 +0200

    zrobienie zad 2

--- lab02/src/zad2.ts
+++ lab02/src/zad2.ts
@@ -1,24 +1,46 @@
-// Fragment kodu do uzupełnienia
 import { questionsChatGpt, Question } from "./questions-chat-gpt";
 
-function topThreeWords(questionsChatGpt: Question[] ): void {
-    const arrayWords: string[] = [];
- 
-    questionsChatGpt.reduce((prev: Question ,curr: Question)=>{
-        arrayWords.push(curr.question)
-    },[])
-
-    console.log(arrayWords)
+interface Record {
+  word: string;
+  count: number;
 }
 
-topThreeWords(questionsChatGpt);
+function countWord(word: string, arr: string[]): number{
+    const result: number = arr.reduce((prev: number,curr: string)=>{
+        if( curr === word)
+            return prev += 1
+        else
+            return prev
+    },0)
+    
+    return result
+}
 
-// const topThree = topThreeWords(questionsChatGpt);
-// console.log(topThree);
+function topThreeWords(questionsChatGpt: Question[]): Record[] {
+  const everyWord: string = questionsChatGpt.reduce( // duży string
+    (prev: string, curr: Question) => {
+      return (prev = prev +" "+ curr.question.replace("?",""));
+    },"");
 
-// Oczekiwany output
-// [
-//   { word: 'Jakie', count: 18 },
-//   { word: 'są', count: 18 },
-//   { word: 'najlepsze', count: 17 }
-// ]
+    const wordArr: string[] = everyWord.replace(" ","").split(" "); //tablica gdzie każdy element to słowo
+
+    const uniqueWords: string[] = wordArr.filter(
+        (val: string,index: number,array: string[])=>{return array.indexOf(val)=== index}) // unikalne słowa
+
+    const countedWords: Record[] = uniqueWords.reduce((prev: Record[],curr: string)=>{ // słowa z policzeniem występowania
+        const newObject: Record = {
+            word: curr,
+            count: countWord(curr,wordArr),
+        }
+
+        return [...prev , newObject]
+        
+    },[])
+
+    const recordsSorted: Record[] = countedWords.sort((a: Record,b: Record) => b.count - a.count)
+
+    return recordsSorted.slice(0,3)
+}
+
+const topThree: Record[] = topThreeWords(questionsChatGpt);
+console.log(topThree);
