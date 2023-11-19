1f858fd412df6a9d5807c980543f60670b9501fd
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-11-19 15:55:28 +0100

    Dodanie pipe

---
+++ Zadanie2/src/app/shared/pipes/animal-data-display.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'animalDataDisplay'
+})
+export class AnimalDataDisplayPipe implements PipeTransform {
+
+  public transform(text: string): string {
+    const firstLetter: string = text.charAt(0).toUpperCase();
+    const rest: string = text.slice(1).toLowerCase();
+
+    return firstLetter + rest;
+  }
+
+}
--- Zadanie2/src/app/shared/shared.module.ts
+++ Zadanie2/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './components/modal/modal.component';
+import { AnimalDataDisplayPipe } from './pipes/animal-data-display.pipe';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    AnimalDataDisplayPipe
   ],
   imports: [
     CommonModule
   ], 
   exports: [
-    ModalComponent
+    ModalComponent,
+    AnimalDataDisplayPipe
   ]
 })
 export class SharedModule { }
