3af6b66f30fd59ebf884d8e5c8e6b41c23981a0f
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-11-26 14:05:52 +0100

    Zad 1

--- lab07/src/app/services/number-emitter.service.ts
+++ lab07/src/app/services/number-emitter.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NumberEmitterService {
+  private numberSubject: Subject<number> = new Subject<number>();
 
   constructor() { }
+
+  public getObservable(): Observable<number>{
+    return this.numberSubject.asObservable();
+  }
+
+  public emitNumber(num: number): void {
+    this.numberSubject.next(num);
+  }
 }
