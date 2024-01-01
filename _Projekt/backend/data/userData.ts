d71d71bc5acab1efe311873d44500c94e1842164
Karol Krawczykiewicz <26karol26@gmail.com>
2024-01-01 22:11:34 +0100

    dodanie user routes

---
+++ _Projekt/backend/data/userData.ts
@@ -0,0 +1,19 @@
+import { User } from "../models/User.model";
+import { v4 as uuidv4 } from 'uuid';
+
+export const userData: User[] = [
+    {
+        id: uuidv4(),
+        firstName: "Karol",
+        lastName: "Krawczykiewicz",
+        email: "k-krawczykiewicz@wp.pl",
+        password: "123"
+    },
+    {
+        id: uuidv4(),
+        firstName: "Admin",
+        lastName: "Testowy",
+        email: "test@test.pl",
+        password: "test"
+    }
+]
